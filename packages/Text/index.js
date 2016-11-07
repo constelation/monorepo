78fb51cc4ef253020343e8cb1270476ac926953a
Kyle Poole <2kylepoole@gmail.com>
2016-11-06 23:18:43 -0800

    flow-typed project. Added proptypes to Text.

--- packages/Text/index.js
+++ packages/Text/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 var React = require('react')
 var glamorReact = require('glamor/react')
 var _omit = require('lodash/omit')
@@ -16,8 +18,30 @@ var textProps = [
   'center'
 ]
 
-function getStyleFromProps( props ) {
-  var style = {
+type? Props = {
+  bold?: bool,
+  underline?: bool,
+  uppercase?: bool,
+  center?: bool,
+  color?: string,
+  decoration?: string,
+  fontFamily?: string,
+  height?: string | number,
+  refNode?: () => {},
+  size?: string | number,
+  spacing?: string | number,
+  tag?: string,
+  css?: Object,
+}
+
+function getStyleFromProps( props: Props ) {
+  const style : {
+    fontWeight?: string,
+    textDecoration?: string,
+    textTransform?: string,
+    textAlign?: string,
+    lineHeight?: string,
+  } = {
     fontFamily: props.fontFamily,
     color: props.color,
     fontSize: props.size,
@@ -68,6 +92,8 @@ function getNonStyleProps( props ) {
 }
 
 class Text extends React.PureComponent {
+  props: Props
+
   static propTypes = {
     children: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.array]),
     bold: React.PropTypes.bool,
