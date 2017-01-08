6667a07bbce4004852b5b28dad26a38bd2e61595
Kyle Poole <2kylepoole@gmail.com>
2017-01-08 00:08:28 -0800

    delete InlineCol/Flex/Row

--- packages/InlineRow/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var displayComponentFactory = require('constelation-display-component-factory')
-
-module.exports = displayComponentFactory('InlineRow', {display: 'inline-flex', flexDirection: 'row'})
