c56e92c96ab30abaacd10537dd340a70a2c9caa2
Kyle Poole <2kylepoole@gmail.com>
2017-02-12 08:01:48 -0800

    add __DEV__ to storybook's webpack

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,5 @@
 // load the default config generator.
+var webpack = require('webpack')
 var genDefaultConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js');
 
 module.exports = function(config, env) {
@@ -6,6 +7,17 @@ module.exports = function(config, env) {
 
   // Extend it as you need.
   config.resolve.alias['react-native'] = 'react-native-web'
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+        __DEV__: true
+        // __PROD__: env.prod || false,
+
+        // Note: `process.env.NODE_ENV` is automatically set with `webpack -p`
+        //       which is needed for build React in prod mode
+      })
+  )
+
 
   return config;
 };
