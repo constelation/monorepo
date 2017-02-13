92068a2f4041b10e6d4335c01d9c04ec01698562
Kyle Poole <2kylepoole@gmail.com>
2017-02-13 08:26:09 -0800

    start of jest tests. just View for now.

---
+++ packages/view-x/__tests__/View.native-test.js
@@ -0,0 +1,38 @@
+import 'react-native'
+import React from 'react'
+import View from '../index.native.js'
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <View />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('alignVertical and alignHorizontal', () => {
+  const view = renderer.create(
+    <View
+      alignVertical='top'
+      alignHorizontal='right'
+    />
+  ).toJSON();
+
+  expect(view.props.style.justifyContent).toBe('flex-start')
+  expect(view.props.style.alignItems).toBe('flex-end')
+});
+
+test('horizontal alignVertical and alignHorizontal', () => {
+  const horizontalView = renderer.create(
+    <View
+      horizontal
+      alignVertical='top'
+      alignHorizontal='right'
+    />
+  ).toJSON();
+
+  expect(horizontalView.props.style.alignItems).toBe('flex-start')
+  expect(horizontalView.props.style.justifyContent).toBe('flex-end')
+});
