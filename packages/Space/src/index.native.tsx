1a543a70809cbf037c01a223e054fb29161c042e
Kyle Poole <2kylepoole@gmail.com>
2017-03-04 19:35:27 -0800

    add constelation-Space

---
+++ packages/Space/src/index.native.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactNative from 'react-native'
+
+export interface IProps {
+  size: number | string,
+}
+
+function getStyleFromProps(props: IProps) {
+  return {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis: props.size,
+  }
+}
+
+export default class Space extends React.PureComponent<IProps, void> {
+  render() {
+    return <ReactNative.View style={getStyleFromProps(this.props)} />
+  }
+}
---
+++ packages/Space/src/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import * as glamorReact from 'glamor-react'
+
+export interface IProps {
+  size: number | string,
+}
+
+function getStyleFromProps(props: IProps) {
+  const flexBasis = (typeof props.size === 'number')
+    ? `${props.size}px`
+    : props.size
+
+  return {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis,
+  }
+}
+
+export default class Space extends React.PureComponent<IProps, void> {
+  render() {
+    return glamorReact.createElement('div', { css: getStyleFromProps(this.props) })
+  }
+}
